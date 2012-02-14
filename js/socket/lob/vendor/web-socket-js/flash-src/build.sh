6f97f4f936d0b6fb7db893ac13f57e29801d9e02
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2012-02-14 12:17:41 +0100

    Remove mice.js and the web socket code

--- js/socket/lob/vendor/web-socket-js/flash-src/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# You need Flex 4 SDK:
-# http://opensource.adobe.com/wiki/display/flexsdk/Download+Flex+4
-
-mxmlc -static-link-runtime-shared-libraries -output=../WebSocketMain.swf WebSocketMain.as
